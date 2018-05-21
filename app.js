3bfe585b2a64fbda122e464b415fcb571775ac32
Nicole <nicole.sattler@live.com>
2018-05-21 18:37:14 -0500

    Require path

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const express = require('express'),
       session = require('express-session'),
       methodOverride = require('method-override'),
       dbRoutes = require('./routes/db'),
-      authRoutes = require('./routes/auth');  
+      authRoutes = require('./routes/auth')
+      path = require('path');
  
 
 // INITIAL SETUP
 require('dotenv').config()
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(methodOverride('_method'));
 
 // http://www.codexpedia.com/node-js/a-very-basic-session-auth-in-node-js-with-express-js/
