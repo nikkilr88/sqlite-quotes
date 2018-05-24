4f22e0e388852051429925d5b8834cbfb585b1c0
Nicole <nicole.sattler@live.com>
2018-05-24 14:22:39 -0500

    Scroll to sections and polyfill

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,22 @@
 const button = document.querySelector('button');
-const howTo = document.querySelector('.howTo');
+const reqBtn = document.querySelector('#request');
+const learnBtn = document.querySelector('#learnMore');
 const logo = document.querySelector('#logo-grey');
 
-button.addEventListener('click', function() {
-   scrollTo(howTo);
-});
+const howTo = document.querySelector('.howTo');
+const request = document.querySelector('.request');
+
+window.__forceSmoothScrollPolyfill__ = true;
+
+[button, learnBtn].map(function(btn) {
+    btn.addEventListener('click', function () {
+        scrollTo(howTo);
+    });
+})
+
+reqBtn.addEventListener('click', function() {
+    scrollTo(request);
+})
 
 logo.addEventListener('click', function () {
     scrollTo(null);
@@ -13,7 +25,7 @@ logo.addEventListener('click', function () {
 function scrollTo(elem) {
     let pos = (!elem) ? 0 : elem.offsetTop;
     window.scroll({
-        top: pos,
+        top: pos - 100,
         behavior: "smooth"
     });
 }
