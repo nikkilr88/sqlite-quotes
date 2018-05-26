023cfc54485103f51971ce6d89464e31d824201e
Nicole <nicole.sattler@live.com>
2018-05-26 12:24:21 -0500

    Update quotes

--- db/quoteData.js
+++ db/quoteData.js
@@ -10,7 +10,7 @@ const quotes = [
         category: "motivational"
     },
     {
-        text: "You can''t cross the sea merely by standing and staring at the water.",
+        text: "You can't cross the sea merely by standing and staring at the water.",
         author: "Carol Burnett",
         category: "motivational"
     },
@@ -32,6 +32,36 @@ const quotes = [
     {
         text: "When life gives you lemons, squirt someone in the eye.",
         author: "Cathy Guisewite",
+        category: "funny"
+    },
+    {
+        text: "Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.",
+        author: "Yoda",
+        category: "tv/movies"
+    },
+    {
+        text: "Do or do not, there is no try.",
+        author: "Yoda",
+        category: "tv/movies"
+    },
+    {
+        text: "Hold the door!",
+        author: "Hodor",
+        category: "tv/movies"
+    },
+    {
+        text: "Winter is coming.",
+        author: "House Stark",
+        category: "tv/movies"
+    },
+    {
+        text: "Dracarys!",
+        author: "Daenerys Targaryen",
+        category: "tv/movies"
+    },
+    {
+        text: "Accept who you are. Unless you’re a serial killer.",
+        author: "Ellen DeGeneres",
         category: "funny"
     }
 ];
