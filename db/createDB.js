88f6755abe969b7d56f438fcfbad894b5db21754
Nicole <nicole.sattler@live.com>
2018-05-26 12:49:53 -0500

    Add col to create table

--- db/createDB.js
+++ db/createDB.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('quotes.db');
 const quoteData = require('./quoteData');
 
 db.serialize(() => {    
-    db.run("CREATE TABLE IF NOT EXISTS quotes (text TEXT, author TEXT, category TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS quotes (text TEXT, author TEXT, category TEXT, last_modified)");
 
     const delAll = new Promise((resolve, reject) => {
         db.run('DELETE FROM quotes', err => {
@@ -16,11 +16,13 @@ db.serialize(() => {
     .then(msg => {
         console.log(msg);
         quoteData.forEach(quote => {
-            db.run('INSERT INTO quotes VALUES ($text,$author,$category)', 
+            db.run('INSERT INTO quotes VALUES ($text,$author,$category,$date)', 
             {
                 $text: quote.text, 
                 $author: quote.author, 
-                $category: quote.category
+                $category: quote.category,
+                $date: Date.now()
+              
             },
             err => {
                 if(err) return console.log(err);
