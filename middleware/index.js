733b21dae833c293d848e623a97fb79ae6ea46ef
Nicole <nicole.sattler@live.com>
2018-05-31 12:21:48 -0500

    Add CORS middleware, organize code

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,11 @@
-const utils = {};
-
-utils.verifyAdmin = (req, res, next) => {
+exports.verifyAdmin = (req, res, next) => {
     if (req.session && req.session.user === process.env.USER && req.session.admin)
         return next();
     else
         return res.sendStatus(401);
 };
 
-module.exports = utils;
+exports.allowCrossOrigin = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+}
